7a4280b5de42e4cdbeb8744c2f52a70353d7a4ae
Mohamed <mohameda249@hotmail.com>
2024-08-19 14:01:10 +0300

    read rfid

--- lib/src/new_card/presentation/app/cubit/nfc_scanner_cubit.dart
+++ lib/src/new_card/presentation/app/cubit/nfc_scanner_cubit.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-// import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
-// import 'package:ndef/ndef.dart' as ndef;
-
 import 'package:fushati/core/utils/core_utils.dart';
 import 'package:fushati/src/new_card/presentation/widgets/nfc_loader.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:nfc_manager/platform_tags.dart';
 
 part 'nfc_scanner_state.dart';
 
@@ -40,11 +37,33 @@ class NfcScannerCubit extends Cubit<NfcScannerState> {
       }, onDiscovered: (NfcTag tag) async {
         /// to remove loader
         Navigator.pop(context);
-        var date = tag.data;
-        debugPrint("NfcTag data: $date");
+        // var date = tag.data;
+        // Assuming the RFID tag is ISO7816
+        final ndef = Ndef.from(tag);
+
+        String _rfidData = '';
+        if (ndef != null) {
+          /// Reading the NDEF message
+          NdefMessage message = ndef.cachedMessage!;
+          final payload = message.records.first.payload;
+          final data = String.fromCharCodes(payload);
+          _rfidData = "RFID Data: $data";
+        } else {
+          /// Handling for non-NDEF tags
+          final iso7816 = Iso7816.from(tag);
+          if (iso7816 != null) {
+            // Perform ISO7816 command and response processing here
+
+            _rfidData = "ISO7816 RFID Tag Detected ${iso7816.toString()}";
+          } else {
+            _rfidData = "Unknown tag detected";
+          }
+        }
+        debugPrint("NfcTag data: $_rfidData");
+
         CoreUtils.showSuccess(
-            title: "TAG READ", subTitle: "TAG DATA $date", onPressed: () {});
-        controller.text = tag.data.length.toString();
+            title: "TAG READ", subTitle: "TAG DATA $_rfidData", onPressed: () {});
+        // End the NFC session after reading
         NfcManager.instance.stopSession();
       });
     } else {
